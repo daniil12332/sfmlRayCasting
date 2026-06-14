3775fde4a11628f29269ddb96e03607c1eab6064
Daniil Petrov <daniil.codi7@gmail.com>
2026-06-14 22:36:00 +0300

    add FOV player

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 using namespace sf;
 
 int main() {
-	RenderWindow window(VideoMode({800, 600}), "Ray casting", Style::Default, State::Fullscreen);
+	RenderWindow window(VideoMode({800, 600}), "Ray casting", Style::Default, State::Windowed);
 	bool mapMode = true;
 
 	Map map;
@@ -18,10 +18,14 @@ int main() {
 	Player player(200, 200, 30.f*M_PI/180);
 	CircleShape playerShape(10.0f);
 
+	float lineX = player.x;
+	float lineY = player.y;
 	std::array viewLines = {
 		Vertex{Vector2f(player.x, player.y)},
-		Vertex{Vector2f(player.x, player.y)},
+		Vertex{Vector2f(lineX, lineY)}
 	};
+	double lineFOVstep = FOV/window.getSize().x;
+	double lineAngle = player.angle-FOV/2;
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
@@ -44,21 +48,18 @@ int main() {
 			window.draw(playerShape);
 		}
 
-		/*for (int rangeToWall = 0; rangeToWall < 10000; rangeToWall++) {
-			for (int wallIndex = 0; wallIndex < map.size(); wallIndex++) {
-
+		lineAngle = player.angle - FOV/2;
+		for (int ray = 0; ray < window.getSize().x; ray++) {
+			for (int rangeToWall = 0; rangeToWall < 200; rangeToWall++) {
+				lineX = player.x + cos(lineAngle)*rangeToWall;
+				lineY = player.y + sin(lineAngle)*rangeToWall;
+				for (int wallIndex = 0; wallIndex < map.size(); wallIndex++) {
+					viewLines[1].position = Vector2f(lineX, lineY);
+					window.draw(viewLines.data(), viewLines.size(), PrimitiveType::Lines);
+				}
 			}
-		}*/
-		int rangeToWall = 200;
-		float lineX = player.x + cos(player.angle-FOV/2)*rangeToWall;
-		float lineY = player.y + sin(player.angle-FOV/2)*rangeToWall;
-		viewLines[1] = Vertex{Vector2f(lineX, lineY)};
-		window.draw(viewLines.data(), viewLines.size(), PrimitiveType::Lines);
-
-		lineX = player.x + cos(player.angle+FOV/2)*rangeToWall;
-		lineY = player.y + sin(player.angle+FOV/2)*rangeToWall;
-		viewLines[1] = Vertex{Vector2f(lineX, lineY)};
-		window.draw(viewLines.data(), viewLines.size(), PrimitiveType::Lines);
+			lineAngle += lineFOVstep;
+		}
 
 		window.display();
 	}
